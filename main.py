a85f4f658ac3bda989d1800833787d25e6fc9c97
Arnob Chanda <arnobchanda@gmail.com>
2025-09-28 20:45:18 +0000

    A working thing?

--- main.py
+++ main.py
@@ -20,10 +20,49 @@ def main():
 
     # The decodebin is using the /dev/imx_hantro device.
     # We need to use imxvideoconvert_g2d device for VPU 2D convert
-    pipeline_str = f"rtspsrc location={rtsp_url} latency=200 ! decodebin ! videoconvert ! v4l2sink device=/dev/video10" #<- This is working.
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=0 ! decodebin ! videoconvert ! v4l2sink device=/dev/video10" #<- This is working.
+    pipeline_str = f"rtspsrc location={rtsp_url} latency=100 do-retransmission=false ! decodebin ! queue max-size-buffers=2 leaky=downstream ! videoconvert ! v4l2sink device=/dev/video10 sync=false"
+    # pipeline_str = (
+    # f"rtspsrc location={rtsp_url} latency=100 do-retransmission=false ! "
+    # "queue max-size-buffers=2 leaky=downstream ! "
+    # "rtph264depay ! h264parse config-interval=-1 ! "
+    # "video/x-h264,stream-format=byte-stream,alignment=au ! "
+    # "vpudec ! videoconvert ! "
+    # "video/x-raw,format=RGB,width=1280,height=720,framerate=25/1 ! "
+    # "v4l2sink device=/dev/video10 sync=false"
+    # )
+
+
+
+
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=200 ! queue max-size-buffers=2 leaky=downstream ! rtph264depay ! h264parse ! vpudec ! queue max-size-buffers=2 ! videoconvert ! video/x-raw,format=YUY2 ! v4l2sink device=/dev/video10 sync=false"
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=200 ! queue max-size-buffers=2 leaky=downstream ! rtph264depay ! h264parse ! vpudec ! queue max-size-buffers=2 ! v4l2sink device=/dev/video10 sync=false"
+    # pipeline_str = f"videotestsrc ! video/x-raw,format=YUY2,width=640,height=480 ! v4l2sink device=/dev/video10"
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=200 ! queue max-size-buffers=2 leaky=downstream ! rtph264depay ! h264parse ! vpudec ! videoconvert ! video/x-raw,format=YUY2,width=640,height=480 ! v4l2sink device=/dev/video10 sync=false"
+    
+    # THis works but gives greenscreen
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=200 ! queue max-size-buffers=2 leaky=downstream ! rtph264depay ! h264parse ! vpudec ! videoconvert ! video/x-raw,format=YUY2,width=1280,height=720 ! v4l2sink device=/dev/video10 sync=false"
+
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=200 ! queue max-size-buffers=2 leaky=downstream ! rtph264depay ! h264parse ! vpudec ! videoconvert ! video/x-raw,format=RGB,width=1280,height=720 ! v4l2sink device=/dev/video10 sync=false"
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=200 ! queue max-size-buffers=2 leaky=downstream ! rtph264depay ! h264parse ! vpudec ! videoconvert ! v4l2sink device=/dev/video10 sync=false"
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=200 ! queue max-size-buffers=2 leaky=downstream ! rtph264depay ! h264parse ! vpudec  ! videoconvert ! capsfilter name=filter ! v4l2sink device=/dev/video10 sync=false"
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=0 ! queue max-size-buffers=2 leaky=downstream ! rtph264depay ! h264parse config-interval=-1 ! capsfilter name=filter ! vpudec  ! videoconvert ! video/x-raw,format=I420,width=1280,height=720 ! v4l2sink device=/dev/video10 sync=false"
+
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=0 ! rtph264depay ! h264parse ! vpudec ! vpuenc_h264 ! h264parse ! v4l2sink device=/dev/video10 sync=false"
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=0 ! rtph264depay ! h264parse ! vpudec ! vpuenc_h264 ! v4l2sink device=/dev/video10 sync=false"
+    
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=0 ! rtph264depay ! h264parse ! vpudec ! vpuenc_hevc ! v4l2sink device=/dev/video10 sync=false"
+    # pipeline_str = f"rtspsrc location={rtsp_url} latency=0 ! rtph264depay ! h264parse ! vpudec ! v4l2sink device=/dev/video10 sync=false"
+
 
     print(f"Creating pipeline: {pipeline_str}")
     pipeline = Gst.parse_launch(pipeline_str)
+    # caps = Gst.Caps.from_string("video/x-h264,stream-format=byte-stream,alignment=au")
+    # caps = Gst.Caps.from_string("video/x-raw,format=NV12,width=1080,height=720,colorimetry=bt709")
+    # filter = pipeline.get_by_name("filter")
+    # filter.set_property("caps", caps)
+
+
 
     # 3. Start the pipeline
     pipeline.set_state(Gst.State.PLAYING)
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export GST_DEBUG=2
+export GST_DEBUG=vpudec:5,decodebin:5
 python3 main.py
